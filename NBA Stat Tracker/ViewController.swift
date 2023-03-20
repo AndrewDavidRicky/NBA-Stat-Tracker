952ae35f90d25209487efc5c081584e66e84be00
Ricky Wong <go.rickyw@gmail.com>
2023-03-20 18:42:56 -0400

    First Commit

---
+++ NBA Stat Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBA Stat Tracker
+//
+//  Created by Ricky Wong on 3/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
